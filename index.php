7f5b50535d1f51b398f19675000269fecc47d01b
박정연 <burningpig9592@gmail.com>
2019-01-01 23:02:39 +0900

    bugfix

--- index.php
+++ index.php
@@ -17,10 +17,10 @@
   */
   function get_api_status_new($host, $name){
     if ($socket =@ fsockopen($host, 80, $errno, $errstr, 30)) {
-      echo "<td>"."<a href=\"https://".$host."\">".$name."</a>"."</td><td><span class=\"badge badge-success\"><i class="fas fa-check"></i> Online</span></td>";
+      echo "<td>"."<a href=\"https://".$host."\">".$name."</a>"."</td><td><span class=\"badge badge-success\"><i class=\"fas fa-check\"></i> Online</span></td>";
       fclose($socket);
     } else {
-      echo "<td>"."<a href=\"https://".$host."\">".$name."</a>"."</td><td><span class=\"badge badge-danger\"><i class="fas fa-times"></i> Offline</span></td>";
+      echo "<td>"."<a href=\"https://".$host."\">".$name."</a>"."</td><td><span class=\"badge badge-danger\"><i class=\"fas fa-times\"></i> Offline</span></td>";
     }
   }  
 ?>
